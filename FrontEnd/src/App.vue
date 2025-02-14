e98b585f13902e56927f05b674ab969d3ad647c8
Provost Aurelien <Provostaurelien@gmail.com>
2025-02-14 11:15:59 +0100

    Initialisation VueJs

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Bonjour {{ firstName.toUpperCase() }}</h1>
+  <p>Comment allez vous??</p>
+</template>
+
+<script setup>
+const firstName = 'John'
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
